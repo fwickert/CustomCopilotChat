b7d70c364cac1b6bc8537bdbc196b6b9ff6a70ce
Frederic Wickert <frederic.wickert@live.fr>
2023-12-05 14:31:31 +0100

    Azure Document Intelligent Service instead Form reco (Model Layout with Markdown)

--- KernelMemory/CoreLib/DataFormats/Image/AzureFormRecognizer/AzureFormRecognizerEngine.cs
+++ KernelMemory/CoreLib/DataFormats/Image/AzureFormRecognizer/AzureFormRecognizerEngine.cs
@@ -5,11 +5,13 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure;
-using Azure.AI.FormRecognizer.DocumentAnalysis;
+using Azure.AI.DocumentIntelligence;
+//using Azure.AI.FormRecognizer.DocumentAnalysis;
 using Azure.Identity;
 using Microsoft.Extensions.Logging;
 using Microsoft.KernelMemory.Configuration;
 using Microsoft.KernelMemory.Diagnostics;
+using Microsoft.KernelMemory.FileSystem.DevTools;
 
 namespace Microsoft.KernelMemory.DataFormats.Image.AzureFormRecognizer;
 
@@ -18,7 +20,8 @@ namespace Microsoft.KernelMemory.DataFormats.Image.AzureFormRecognizer;
 /// </summary>
 public class AzureFormRecognizerEngine : IOcrEngine
 {
-    private readonly DocumentAnalysisClient _recognizerClient;
+    //private readonly DocumentAnalysisClient _recognizerClient;
+    private readonly DocumentIntelligenceClient _documentIntelligenceClient;
     private readonly ILogger<AzureFormRecognizerEngine> _log;
 
     /// <summary>
@@ -35,7 +38,8 @@ public class AzureFormRecognizerEngine : IOcrEngine
         switch (config.Auth)
         {
             case AzureFormRecognizerConfig.AuthTypes.AzureIdentity:
-                this._recognizerClient = new DocumentAnalysisClient(new Uri(config.Endpoint), new DefaultAzureCredential());
+                //this._recognizerClient = new DocumentAnalysisClient(new Uri(config.Endpoint), new DefaultAzureCredential());
+                this._documentIntelligenceClient = new DocumentIntelligenceClient(new Uri(config.Endpoint), new DefaultAzureCredential());
                 break;
 
             case AzureFormRecognizerConfig.AuthTypes.APIKey:
@@ -45,7 +49,8 @@ public class AzureFormRecognizerEngine : IOcrEngine
                     throw new ConfigurationException("Azure Form Recognizer API key is empty");
                 }
 
-                this._recognizerClient = new DocumentAnalysisClient(new Uri(config.Endpoint), new AzureKeyCredential(config.APIKey));
+                //this._recognizerClient = new DocumentAnalysisClient(new Uri(config.Endpoint), new AzureKeyCredential(config.APIKey));
+                this._documentIntelligenceClient = new DocumentIntelligenceClient(new Uri(config.Endpoint), new AzureKeyCredential(config.APIKey));
                 break;
 
             default:
@@ -58,11 +63,86 @@ public class AzureFormRecognizerEngine : IOcrEngine
     public async Task<string> ExtractTextFromImageAsync(Stream imageContent, CancellationToken cancellationToken = default)
     {
         // Start the OCR operation
-        var operation = await this._recognizerClient.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-read", imageContent, cancellationToken: cancellationToken).ConfigureAwait(false);
+        //var operation = await this._recognizerClient.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-read", imageContent, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+        var content = new AnalyzeDocumentContent()
+        {
+            Base64Source = new BinaryData(imageContent.ReadAllBytes())
+        };
+
+        
+        
+
+        var operation = await this._documentIntelligenceClient.AnalyzeDocumentAsync
+            (WaitUntil.Completed, "prebuilt-layout", content, null, null, null, null, null,
+            ContentFormat.Markdown,cancellationToken: cancellationToken).ConfigureAwait(false);
+
 
         // Wait for the result
         Response<AnalyzeResult> operationResponse = await operation.WaitForCompletionAsync(cancellationToken).ConfigureAwait(false);
 
+        //Le Value.content n'est pas structuré. Alors on va prendre les KeyValuesPairs + Paragraphs + Tables pour structurer
+        // Exemple de structure : Informations de devis, Le devis, autres informations
+
+        //StringBuilder stringBuilder = new StringBuilder();
+        //stringBuilder.AppendLine("[START Informations de devis]");
+
+        //foreach (AnalyzedDocument doc in operation.Value.Documents)
+        //{
+        //    foreach (KeyValuePair<string, DocumentField> field in doc.Fields)
+        //    {
+        //        switch (field.Value.FieldType)
+        //        {
+        //            case DocumentFieldType.List:
+        //                foreach (DocumentField item in field.Value.Value.AsList())
+        //                {
+        //                    stringBuilder.AppendLine("Items: ");
+        //                    if (item.FieldType == DocumentFieldType.Dictionary)
+        //                    {
+        //                        foreach (KeyValuePair<string, DocumentField> itemField in item.Value.AsDictionary())
+        //                        {
+        //                            if (itemField.Value.FieldType == DocumentFieldType.String)
+        //                            {
+        //                                stringBuilder.AppendLine($"{itemField.Key} : {itemField.Value.Content}");
+        //                            }
+        //                            if (itemField.Value.FieldType == DocumentFieldType.Currency)
+        //                            {
+        //                                CurrencyValue itemAmount = itemField.Value.Value.AsCurrency();
+        //                                stringBuilder.AppendLine($"{itemField.Key} : {itemAmount.Symbol}{itemAmount.Amount}");
+        //                            }
+        //                        }
+        //                    }
+        //                    else
+        //                    {
+        //                        stringBuilder.AppendLine($"{item.Content}");
+        //                    }
+        //                }
+        //                break;
+        //            default:
+        //                stringBuilder.AppendLine($"{field.Key} : {field.Value.Content}");
+        //                break;
+        //        }
+
+        //    }
+
+        //    stringBuilder.AppendLine("[END Informations de devis]");
+
+
+        //    foreach (DocumentTable table in operation.Value.Tables)
+        //    {
+        //        stringBuilder.AppendLine("[START Table]");
+        //        foreach (DocumentTableCell cell in table.Cells)
+        //        {
+        //            stringBuilder.AppendLine($"[C{cell.ColumnIndex}:R{cell.RowIndex}]={cell.Content}");
+        //        }
+        //        stringBuilder.AppendLine("[END Table]");
+        //    }
+        //}
+
+
+
+
         return operationResponse.Value.Content;
+        //return stringBuilder.ToString(); ;
     }
 }
